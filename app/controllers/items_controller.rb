47258643f9296006bafe0d2d737006967a835c04
sakamo09 <teamilkyui326@gmail.com>
2022-06-20 18:27:14 +0900

    バリデーションの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,4 +5,6 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
   end
+
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,10 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  validates :genre_id, presence: true
+  validates :maker_id, presence: true
+  validates :price, presence: true
+  validates :merit, presence: true
+  validates :image, presence: true
 end
