b1ab080be84e87f650dd0f269a07b0b1260c0292
sakamo09 <teamilkyui326@gmail.com>
2022-06-21 14:32:37 +0900

    Update item.rb

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,8 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+  has_many :comments, dependent: :destroy
+
 
   validates :genre_id, presence: true
   validates :maker_id, presence: true
