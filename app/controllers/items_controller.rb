53945c63fac26c791a15a1f20c8641ce09e38d73
sakamo09 <teamilkyui326@gmail.com>
2022-06-21 14:47:30 +0900

    itemコントローラー編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,12 @@
 class ItemsController < ApplicationController
+  before_action :set_prototype, except: [:index, :new, :create]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :contributor_confirmation, only: [:edit, :update, :destroy]
+  
+  
+  
   def index
+    @items = Item.Includes(:user)
   end
 
   def new
@@ -7,11 +14,52 @@ class ItemsController < ApplicationController
   end
 
   def create
-    @item = Item.new
+    @item = Item.new(item_params)
     if @prototype.save
       redirect_to root_path
     else
       render :new
     end
   end
+
+  def show
+    @comment = Comment.new
+    @comments = @item.comments
+  end
+
+  def edit
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to item_path(@item)
+    else
+      render :edit
+    end
+  end
+  
+  def destroy
+    if @item.destroy
+      redirect_to root_path
+    else
+      redirect_to root_path
+    end
+  end
+
+
+  private
+
+  def item_params
+    params.require(:item).permit(:genre_id, :maker_id, :price, :merit, :image).merge(user_id: current_user.id)
+  end
+
+  def set_prototype
+    @item = Item.find(params[:id])
+  end
+
+  def contributor_confirmation
+    redirect_to root_path unless current_user == @item.user
+  end
+
+
 end
