6747fcf92c07ebd4b717a790e7bdc499ca19b21f
sakamo09 <teamilkyui326@gmail.com>
2022-06-23 15:57:15 +0900

    マイグレーションファイルの修正

--- db/migrate/20220618120818_create_items.rb
+++ db/migrate/20220618120818_create_items.rb
@@ -3,7 +3,7 @@ class CreateItems < ActiveRecord::Migration[6.0]
     create_table :items do |t|
       t.string :genre_id, null: false
       t.string :maker_id, null: false
-      t.string :price, null: false
+      t.integer :price, null: false
       t.text :merit, null: false
       t.references :user, null: false, foreign_key: true
       t.timestamps
